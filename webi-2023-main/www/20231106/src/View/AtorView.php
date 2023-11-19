0aa9ba41c01704e210627ebcf48239bf528e969f
Mateus Dallacourt <66923795+matshw@users.noreply.github.com>
2023-11-19 16:30:37 -0400

    sem index

--- webi-2023-main/www/20231106/src/View/AtorView.php
+++ webi-2023-main/www/20231106/src/View/AtorView.php
@@ -1,44 +1,53 @@
-<style>
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
+<?php
+require_once '../Controller/AtorController.php';
+require_once '../Model/Ator.php';
 
-    th, td {
-        border: 1px solid #dddddd;
-        text-align: left;
-        padding: 8px;
-    }
+$atorController = new AtorController();
 
-    tr:nth-child(even) {
-        background-color: #f2f2f2;
-    }
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+    $nome = $_POST['nome'];
+    $nacionalidade = $_POST['nacionalidade'];
+    $dataNascimento = $_POST['dataNascimento']; 
 
-    .button-group {
-        display: flex;
-    }
+    $novoAtor = new Ator(null, $nome, $nacionalidade, $dataNascimento);
+    $atorController->adicionarAtor($novoAtor);
+    header("Location: {$_SERVER['PHP_SELF']}");
+    exit();
+}
 
-    .button-group button {
-        margin-right: 5px;
-    }
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['excluir'])) {
+    $idAtor = $_POST['excluir'];
+    $atorController->excluirAtor($idAtor);
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['editar'])) {
+    $id = $_POST['editar'];
+    $nome = $_POST['nome_edit'];
+    $nacionalidade = $_POST['nacionalidade_edit'];
+    $dataNascimento = $_POST['dataNascimento_edit'];
 
-    .edit-form {
-        background-color: #f9f9f9;
-        padding: 10px;
-        border-radius: 5px;
-        margin-top: 10px;
-    }
+    $atorController->atualizarAtor($id, $nome, $nacionalidade, $dataNascimento);
 
-    .edit-form label {
-        display: block;
-        margin-bottom: 5px;
-    }
+    header("Location: {$_SERVER['PHP_SELF']}");
+    exit();
+}
 
-    .edit-form input {
-        margin-bottom: 10px;
-    }
-</style>
+?>
 
+<html>
+<body>
+<h1>Adicionar Ator</h1>
+    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required><br><br>
+
+        <label for="nacionalidade">Nacionalidade:</label>
+        <input type="text" id="nacionalidade" name="nacionalidade" required><br><br>
+
+        <label for="dataNascimento">Data de Nascimento:</label>
+        <input type="date" id="dataNascimento" name="dataNascimento" required><br><br>
+
+        <input type="submit" name="submit" value="Adicionar Ator">
+    </form>
 <h2>Lista de Atores</h2>
 <table>
     <thead>
@@ -106,3 +115,5 @@
         }
     }
 </script>
+</body>
+</html>
